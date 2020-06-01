f3281d660453f18c9a7b3fbabe2e917561c97971
adipoluri <adipoluri@gmail.com>
2020-06-01 03:13:07 -0700

    Added Functionality to the settings menu, created audio channels for Gameplay and Music. Added Soundeffects to button clicks.

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonSound : MonoBehaviour
+{
+    
+  
+    public AudioSource audioSource;
+    public AudioClip hoverAudio;
+
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        audioSource.clip = hoverAudio;
+        audioSource.playOnAwake = false;
+
+
+    }
+
+    
+    public void HoverSound () {
+        audioSource.PlayOneShot (hoverAudio);
+    }
+}
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start () {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++) {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
+    
+    public void SetGameplayVolume (float volume) {
+        audioMixer.SetFloat("Gameplay", volume);
+    }
+
+    public void SetMusicVolume (float volume) {
+        audioMixer.SetFloat("Music", volume);
+    }
+
+
+    public void SetQuality (int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution (int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullScreen) {
+        Screen.fullScreen = isFullScreen;
+    }
+}
