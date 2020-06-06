04e0651de556581205c932339d28b2d3757adc19
adipoluri <adipoluri@gmail.com>
2020-06-06 01:33:30 -0700

    Created Loading Screen

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+
+    public TMP_Text timer;
+    private int i = 0;
+    private int endTimer = 1000;
+
+    // Update is called once per frame
+    void FixedUpdate() {
+
+        timer.text = "00:0" + ((endTimer - i)/100).ToString() + ":" + ((endTimer - i)).ToString();
+        i++;
+
+        if (i > 1000) {
+            SceneManager.LoadScene("Interchange");
+        } 
+    }
+}
